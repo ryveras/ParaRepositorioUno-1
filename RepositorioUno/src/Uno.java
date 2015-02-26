5b7d5642d69e352b809cef03943380bb8c20e286
JuananGF <ja_garcia_23@hotmail.com>
2015-02-26 13:08:38 +0100

    cambio

---
+++ RepositorioUno/src/Uno.java
@@ -0,0 +1,18 @@
+
+public class Uno {
+int propUno;
+int propDos;
+int propTres;
+int propCuatro;
+int propSeis;
+int propOcho;
+int propCinco;
+int propSiete;
+<<<<<<< HEAD
+int propNueve;
+String cambio;
+=======
+int propsses;
+>>>>>>> branch 'master' of https://github.com/JuananGF/ParaRepositorioUno.git
+
+}
